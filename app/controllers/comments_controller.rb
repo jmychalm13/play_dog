e1b833462a9e257c8f95b4d8cc8b93c5c39cb292
“Jessica <jmychalm13@gmail.com>
2024-01-04 19:00:17 -0600

    added update and delete for comments and tests passing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,40 @@ class CommentsController < ApplicationController
     @comments = Comment.all
     render :index
   end
+
+  def create
+    @comment = Comment.create(
+      content: params[:content],
+      user_id: params[:user_id],
+      playdate_id: params[:playdate_id],
+    )
+    if @comment.valid?
+      render :show
+    else
+      render json: { message: "There was an error." }
+    end
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+    render :show
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(
+      content: params[:content] || comment.content
+    )
+    if @comment.valid?
+      render :show
+    else
+      render json: { message: "There was an error updating" }
+    end
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    render json: { message: "Comment successfully deleted." }
+  end
 end
--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,11 +1,54 @@
 require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @comment = Comment.create(
+      content: "I am a test comment.",
+        user_id: User.first.id,
+        playdate_id: Playdate.first.id
+    )
+  end
+
   test "index" do
     get "/comments.json"
     assert_response 200
 
     data = JSON.parse(response.body)
     assert_equal Comment.count, data.length
+  end
+
+  test "create" do
+    assert_difference "Comment.count", 1 do
+      post "/comments.json", params: {
+        content: "I am a test comment.",
+        user_id: User.first.id,
+        playdate_id: Playdate.first.id
+      }
+    end
+  end
+
+  test "show" do
+    get "/comments/#{Comment.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "content", "user_id", "playdate_id"], data.keys
+  end
+
+  test "update" do
+    patch "/comments/#{Comment.first.id}.json", params: {
+      content: "updated content"
+    }
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal "updated content", data["content"]
+  end
+
+  test "destroy" do
+    assert_difference "Comment.count", -1 do
+      delete "/comments/#{Dog.first.id}.json"
+      assert_response 200
+    end
   end
 end
