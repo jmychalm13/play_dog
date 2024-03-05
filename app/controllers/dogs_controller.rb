9510c9aced824ebb52b76b0ec4c1b0c2180f549e
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-03-05 15:43:50 -0600

    added logic to prevent user from adding two dogs with same name - trying to prevent trash data and duplicates - will refactor in the future

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -6,19 +6,28 @@ class DogsController < ApplicationController
   end
 
   def create
-    @dog = Dog.create(
-      name: params[:name],
-      age: params[:age],
-      breed: params[:breed],
-      user_id: params[:user_id],
-      image_url: params[:image_url]
-    )
+    user_id = params[:user_id]
 
-    if @dog.valid?
-      render :show
+    existing_dog = Dog.find_by(user_id: user_id, name: params[:name])
+
+    if existing_dog
+      render json: {error: "There is already a pet with that name in the database"}
     else
-      render json: { message: "There was an error adding this pup" }
+      @dog = Dog.create(
+        name: params[:name],
+        age: params[:age],
+        breed: params[:breed],
+        user_id: params[:user_id],
+        image_url: params[:image_url]
+      )
+  
+      if @dog.valid?
+        render :show
+      else
+        render json: { message: "There was an error adding this pup" }
+      end
     end
+
   end
 
   def show
