0a74338fd990b4bd6e1a8bcbe4c0e85f6d6727b2
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-04-14 11:41:44 -0500

    using Faker gem to create dogs and ensure that each created user has at least one

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,76 +1,5 @@
 puts "Seeding started"
 
-# Dog.create!([
-#   [
-#   {
-#     "name": "Max",
-#     "age": 2,
-#     "breed": "Siberian Husky",
-#     "user_id": 1,
-#   },
-#   {
-#     "name": "Bella",
-#     "age": 3,
-#     "breed": "Dachshund",
-#     "user_id": 2,
-#   },
-#   {
-#     "name": "Cooper",
-#     "age": 1,
-#     "breed": "Shih Tzu",
-#     "user_id": 4,
-#   },
-#   {
-#     "name": "Lola",
-#     "age": 4,
-#     "breed": "Boxer",
-#     "user_id": 5,
-#   },
-#   {
-#     "name": "Ruby",
-#     "age": 2,
-#     "breed": "Cavalier King Charles Spaniel",
-#     "user_id": 6,
-#   },
-#   {
-#     "name": "Rocky",
-#     "age": 3,
-#     "breed": "Pug",
-#     "user_id": 7,
-#   },
-#   {
-#     "name": "Milo",
-#     "age": 1,
-#     "breed": "Poodle",
-#     "user_id": 1,
-#   },
-#   {
-#     "name": "Charlie",
-#     "age": 2,
-#     "breed": "Labrador Retriever",
-#     "user_id": 2,
-#   }
-# ]
-
-# ])
-
-# Create an array of dog behaviors
-# behaviors = ['friendly', 'vocal', 'playful', 'curious', 'energetic', 'shy', 'loyal', 'stubborn', 'gentle']
-
-# # Seed the Behavior model with 10 items
-# 10.times do
-#   # Randomly select a dog_id from the given numbers
-#   dog_id = [2, 3, 4, 5, 6, 7].sample
-
-#   # Randomly select behaviors (you can have more than one behavior per id)
-#   selected_behaviors = behaviors.sample(rand(1..behaviors.length))
-
-#   # Create Behavior records
-#   selected_behaviors.each do |behavior|
-#     Behavior.create(dog_id: dog_id, behavior: behavior)
-#   end
-# end
-
 # 10.times do
 #   user = User.new(
 #     name: Faker::Name.name,
@@ -82,15 +11,24 @@ puts "Seeding started"
 #   user.save
 # end
 
-10.times do
-  user_ids = User.pluck(:id)
-  dog = Dog.new(
+user_ids = User.pluck(:id)
+# 10.times do
+#   dog = Dog.new(
+#     name: Faker::Creature::Dog.name,
+#     breed: Faker::Creature::Dog.breed,
+#     age: rand(1..12),
+#     user_id: user_ids.sample
+#   )
+#   dog.save
+# end
+
+user_ids.each do |user_id|
+  Dog.create(
     name: Faker::Creature::Dog.name,
     breed: Faker::Creature::Dog.breed,
     age: rand(1..12),
-    user_id: user_ids.sample
-  )
-  dog.save
+    user_id: user_id
+  ) unless Dog.exists?(user_id: user_id)
 end
 
 
