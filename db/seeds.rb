6abc3e61ef399607dd4badc191fcbf4620427c5d
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-04-14 12:01:05 -0500

    recreated and seeded new database, added pics to dogs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,26 @@
 puts "Seeding started"
 
-# 10.times do
-#   user = User.new(
-#     name: Faker::Name.name,
-#     email: Faker::Internet.email,
-#     image_url: "https://picsum.photos/200/300?random=#{rand(1..1000)}"
-#   )
-#   user.password = 'password'
-#   user.password_confirmation = 'password'
-#   user.save
-# end
+10.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    image_url: "https://picsum.photos/200/300?random=#{rand(1..1000)}"
+  )
+  user.password = 'password'
+  user.password_confirmation = 'password'
+  user.save
+end
 
 user_ids = User.pluck(:id)
-# 10.times do
-#   dog = Dog.new(
-#     name: Faker::Creature::Dog.name,
-#     breed: Faker::Creature::Dog.breed,
-#     age: rand(1..12),
-#     user_id: user_ids.sample
-#   )
-#   dog.save
-# end
+10.times do
+  dog = Dog.new(
+    name: Faker::Creature::Dog.name,
+    breed: Faker::Creature::Dog.breed,
+    age: rand(1..12),
+    user_id: user_ids.sample
+  )
+  dog.save
+end
 
 user_ids.each do |user_id|
   Dog.create(
